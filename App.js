179eb84366d01df284e7713fc15cc44b08f3c35b
matheusvieira300 <53275513+matheusvieira300@users.noreply.github.com>
2023-12-07 12:10:00 -0300

    Terminada Parte 2 - Imagens e fonts do APP

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import Cesta from './src/telas/Cesta';//tem que importar senão ele não reconhece porém se atentar pois ele não desmonstra erro no código
 
-export default function App() {
+import { useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold } from '@expo-google-fonts/montserrat'; //importando a fonte montserrat do google
+
+export default function App() {// componente
+const [fonteCarregada] = useFonts({
+  "MontserratRegular": Montserrat_400Regular,// definindo um nome para o uso da fonte
+  "MontserratBold": Montserrat_700Bold,
+}); //chamando a fonte
+
+if(!fonteCarregada){//se a fonte não estiver carregada
+  return <View />// enquanto a fonte não estiver carregada vai ser retornada uma tela em branco
+}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+       {/* Safe Area View style propriedade e auto valor da propriedade */}
+      <StatusBar/>
+      <Cesta/>   
+      {/*estrutura xml JSX */}
+      {/* View componente  */}
+    </SafeAreaView> 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

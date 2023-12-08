3820a13cdc43e8d27aec3e565d763611e33136c3
matheusvieira300 <53275513+matheusvieira300@users.noreply.github.com>
2023-12-08 14:09:37 -0300

    Criado lista com map

--- src/telas/Cesta/componentes/Itens.js
+++ src/telas/Cesta/componentes/Itens.js
@@ -5,15 +5,15 @@ import Texto from "../../../componentes/Texto";
 
 export default function Itens({titulo, lista}){
    return <>
-        <Texto>{ titulo }</Texto>
+        <Texto style={estilos.titulo}>{ titulo }</Texto>
         {/* map para percorrer uma lista de array e assim pode ser alterado para */}
         {/* retornar uma nova lista */}
         {lista.map(({nome, imagem}) => {
 {/* o atributo key serve para dizer qual vai ser o atributo único/id 
 da lista que não se repete */}
-           return <View>
-            <Texto key={nome}>{ nome }</Texto>
-            <Image source={imagem}/>
+           return <View key={nome} style={estilos.item}>
+            <Image source={imagem} style={estilos.imagem}/>
+            <Texto style={estilos.nome}>{ nome }</Texto>
            </View>
         })}
     </>
@@ -21,6 +21,28 @@ da lista que não se repete */}
 
 const estilos = StyleSheet.create({
     titulo: {
-        color: "#44646"
-    }
-})
+        color: "#44646",
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32
+    },
+    item:{
+        flexDirection: "row", //para ficar um do lado do outro
+        borderBottomWidth: 1,//tem que defininir cado elemento da borda, nesse caso a largura
+        borderBottomColor: "#ECECEC", //cor da borda 
+        paddingVertical: 16, //para dar espaçamento de cima pra baixo
+        alignItems: "center", //alinhar items no centro
+    },
+    imagem:{
+        width: 46,//largura da imagem
+        height: 46 //altura
+    },
+    nome:{
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 11,
+        color:"#464646",
+    },
+}); 
